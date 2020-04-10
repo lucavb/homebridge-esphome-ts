b72555c932b2acba4e6ed5bbcdd809e6823afe2e
Luca Becker <luca.becker@me.com>
2020-04-10 11:14:30 +0200

    modules appear no longer necessary

--- src/moduleDeclaration/bonjour-hap.ts
+++
@@ -1,8 +0,0 @@
-declare module 'bonjour-hap' {
-    export const BonjourHap: any;
-    export const MulticastOptions: any;
-    export const Service: any;
-    export type BonjourHap = any;
-    export type MulticastOptions = any;
-    export type Service = any;
-}
--- src/moduleDeclaration/fast-srp-hap.ts
+++
@@ -1,6 +0,0 @@
-declare module 'fast-srp-hap' {
-    export const Server: any;
-    export const SrpParams: any;
-    export type Server = any;
-    export type SrpParams = any;
-}
