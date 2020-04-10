f006cdd3ce7b69d166ead8b5e523089ae0599932
Luca Becker <luca.becker@me.com>
2020-04-10 11:14:54 +0200

    began implementing sensor component

--- src/homebridgeAccessories/sensor.ts
+++ src/homebridgeAccessories/sensor.ts
@@ -1,6 +1,34 @@
 import {HomebridgePlatformAccessory} from 'homebridge-ts-helper';
 import {SensorComponent} from 'esphome-ts/dist';
+import {Characteristic, Service} from 'hap-nodejs';
+import {
+    CurrentRelativeHumidity,
+    CurrentTemperature,
+    HumiditySensor,
+    TemperatureSensor,
+} from 'hap-nodejs/dist/lib/gen/HomeKit';
+import {tap} from 'rxjs/operators';
+
+const isTemperatureComponent = (unitOfMeasurement: unknown) => unitOfMeasurement === '°C' || unitOfMeasurement === '°F';
 
 export const sensorHelper = (component: SensorComponent, accessory: HomebridgePlatformAccessory): void => {
+    if (isTemperatureComponent(component.unitOfMeasurement)) {
+        defaultSetup(component, accessory, TemperatureSensor, Characteristic.CurrentTemperature);
+    } else if (component.unitOfMeasurement === '%' && component.icon === 'mdi:water-percent') {
+        defaultSetup(component, accessory, HumiditySensor, CurrentRelativeHumidity);
+    }
+};
 
+const defaultSetup = (component: SensorComponent,
+                      accessory: HomebridgePlatformAccessory,
+                      SelectedService: typeof TemperatureSensor | typeof HumiditySensor,
+                      SelectedCharacteristic: typeof CurrentTemperature | typeof CurrentRelativeHumidity): void => {
+    let temperatureSensor: Service | undefined = accessory.services.find((service) => service.UUID === SelectedService.UUID);
+    if (!temperatureSensor) {
+        temperatureSensor = accessory.addService(new SelectedService(component.name, ''));
+    }
+
+    component.state$.pipe(
+        tap(() => temperatureSensor?.getCharacteristic(SelectedCharacteristic)?.setValue(component.value!)),
+    ).subscribe();
 };
