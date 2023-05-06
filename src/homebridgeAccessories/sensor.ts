bae49b4e3143abdfe435475f6923476978f5d6f8
seiry <4397354+seiry@users.noreply.github.com>
2023-05-07 00:38:34 +0800

    add air level

--- src/homebridgeAccessories/sensor.ts
+++ src/homebridgeAccessories/sensor.ts
@@ -1,7 +1,8 @@
+import { SensorComponent } from 'esphome-ts';
+import { AirQualitySensor } from 'hap-nodejs/dist/lib/definitions';
+import { CharacteristicValue, PlatformAccessory, Service as HAPService } from 'homebridge';
 import { tap } from 'rxjs/operators';
 import { Characteristic, Service } from '../index';
-import { SensorComponent } from 'esphome-ts';
-import { CharacteristicValue, PlatformAccessory, Service as HAPService } from 'homebridge';
 
 const isTemperatureComponent = (unitOfMeasurement: unknown) => unitOfMeasurement === '°C' || unitOfMeasurement === '°F';
 
@@ -17,7 +18,7 @@ export const sensorHelper = (component: SensorComponent, accessory: PlatformAcce
         return true;
     } else if (component.unitOfMeasurement === 'µg/m³') {
         // && component.icon === 'mdi:air-filter'
-        defaultSetup(component, accessory, Service.AirQualitySensor, Characteristic.PM2_5Density);
+        airQualitySetup(component, accessory);
         return true;
     }
     return false;
@@ -26,11 +27,8 @@ export const sensorHelper = (component: SensorComponent, accessory: PlatformAcce
 const defaultSetup = (
     component: SensorComponent,
     accessory: PlatformAccessory,
-    SelectedService: typeof Service.TemperatureSensor | typeof Service.HumiditySensor | typeof Service.AirQualitySensor,
-    SelectedCharacteristic:
-        | typeof Characteristic.CurrentTemperature
-        | typeof Characteristic.CurrentRelativeHumidity
-        | typeof Characteristic.PM2_5Density,
+    SelectedService: typeof Service.TemperatureSensor | typeof Service.HumiditySensor,
+    SelectedCharacteristic: typeof Characteristic.CurrentTemperature | typeof Characteristic.CurrentRelativeHumidity,
 ): void => {
     let targetSensor: HAPService | undefined = accessory.services.find(
         (service) => service.UUID === SelectedService.UUID,
@@ -47,6 +45,42 @@ const defaultSetup = (
                     ?.getCharacteristic(SelectedCharacteristic)
                     ?.setValue(component.value! as CharacteristicValue),
             ),
+        )
+        .subscribe();
+};
+const airQualitySetup = (component: SensorComponent, accessory: PlatformAccessory): void => {
+    let targetSensor: HAPService | undefined = accessory.services.find(
+        (service) => service.UUID === AirQualitySensor.UUID,
+    );
+    if (!targetSensor) {
+        targetSensor = accessory.addService(new AirQualitySensor(component.name, ''));
+    }
+
+    component.state$
+        .pipe(
+            // @ts-ignore
+            tap(() => {
+                const pm25 = component.value;
+                let level: number;
+                // https://www.epa.gov/sites/default/files/2016-04/documents/2012_aqi_factsheet.pdf
+                // https://developers.homebridge.io/#/characteristic/AirQuality
+                if (!pm25) {
+                    level = Characteristic.AirQuality.UNKNOWN;
+                } else if (pm25 <= 12) {
+                    level = Characteristic.AirQuality.EXCELLENT;
+                } else if (pm25 <= 35) {
+                    level = Characteristic.AirQuality.GOOD;
+                } else if (pm25 <= 55) {
+                    level = Characteristic.AirQuality.FAIR;
+                } else if (pm25 <= 150) {
+                    level = Characteristic.AirQuality.INFERIOR;
+                } else {
+                    level = Characteristic.AirQuality.POOR;
+                }
+
+                targetSensor?.getCharacteristic(Characteristic.PM2_5Density)?.setValue(component.value!);
+                targetSensor?.getCharacteristic(Characteristic.AirQuality)?.setValue(level);
+            }),
         )
         .subscribe();
 };
